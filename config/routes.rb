7839c12e734350c675d4c2b80cf22077f2dfc190
Cameron <cdsteer94@gmail.com>
2015-06-11 11:23:04 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-
-
   resources :articles do
     resources :comments
   end
   
+  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
