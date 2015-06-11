54a100477f61fc393ef1c7e7d7decda418d85aa7
Cameron <cdsteer94@gmail.com>
2015-06-11 12:49:48 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   
-  resources :users
+  get "articles/"
+  get "articles/new"
+
   resources :articles do
     resources :comments
   end
