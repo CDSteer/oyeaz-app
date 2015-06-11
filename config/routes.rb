bc068f377c3be643dbbaa00a654b62b96d260226
Cameron <cdsteer94@gmail.com>
2015-06-11 12:56:42 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-  
-  get "articles/"
-  get "articles/new"
+
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
 
   resources :articles do
     resources :comments
   end
-  
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
+  resources :users
+  resource :session
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
 
 
