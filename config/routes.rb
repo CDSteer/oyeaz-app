f01601fefd582f561a51b29d85cb9faf03e03810
Cameron <cdsteer94@gmail.com>
2015-06-11 11:17:06 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get 'welcome#index'
+
 
   resources :articles do
     resources :comments
   end
   
+  root 'welcome#index'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
