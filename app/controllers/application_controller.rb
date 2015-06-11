682e198dcd1462ab7315b63d17fe049fefb58365
Cameron <cdsteer94@gmail.com>
2015-06-11 13:04:06 +0100

    fixed blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	def new
+	end
 
-  private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+	def create
+		render plain: params[:article].inspect
+	end
+
+	private
+		def current_user
+			@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		end
+	helper_method :current_user
 end
