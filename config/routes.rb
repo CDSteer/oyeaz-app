7c2dc617384fbff417819f601f16c68de47acd0d
Cameron <cdsteer94@gmail.com>
2015-06-11 11:05:34 +0100

    code

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-  # get 'auth/:provider/callback', to: 'sessions#create'
-  # get 'logout', to: 'sessions#destroy'
-
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'layouts#application'
 
   resources :articles do
     resources :comments
   end
   
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
 
   # Example of regular route:
