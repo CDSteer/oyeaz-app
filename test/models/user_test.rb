1606ba76fba16d21a0b59bb53e8eaec7ffdb2c53
Cameron <cdsteer94@gmail.com>
2015-06-29 20:35:50 +0100

    stuff

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert true
+  end
+
+	# test "has location" do
+	# 	user = User.create
+	# 	2.times {user.location.create}
+	# 	assert_equal(2, user.location.count, "Unexpected associated records count")
+	# end
 end
