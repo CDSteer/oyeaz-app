f1f7ef24ece761274d32f0f10a92ddac041c53fd
Cameron <cdsteer94@gmail.com>
2015-06-29 15:42:09 +0100

    web based user location updateing & deleting

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,24 +1,52 @@
 class LocationsController < ApplicationController
 
-	# def index
-	# 	@location = Location.all
-	# 	# @location = Location.find(params[:user_id])
-	# end
+	def index
+		
+	end
 
 	def show
-		@user = User.find(params[:user_id])
+		if current_user
+			@user = User.find(params[:user_id])
+		else 
+			redirect_to root_path
+		end 
 	end
 
 	def new
-		getLocation
-		@user = User.find(params[:user_id])
-		@location = Location.create(:latitude => @latitude, :longitude => @longitude, :user_id => @user.id)
-		@location.save
-		# redirect_to location_path(@locations)
+		if current_user
+			getLocation
+			@user = User.find(params[:user_id])
+			@location = Location.create(:latitude => @latitude, :longitude => @longitude, :user_id => @user.id)
+			@location.save
+		else
+			redirect_to root_path
+		end
+	end
+
+	def update
+		if current_user
+			getLocation
+			@location = Location.find(params[:user_id])
+			@location.latitude = @latitude
+			@location.longitude = @longitude
+			@location.update
+			redirect_to user_path
+		else
+			redirect_to root_path
+		end
 	end
  
-  private
+ 	def destroy
+ 		if current_user
+			@location = Location.find(params[:user_id])
+			@location.destroy
+			redirect_to articles_path
+		else
+			redirect_to root_path
+		end
+	end
 
+  private
     def getLocation
 		@lat_lng = cookies[:lat_lng].split("|")
 		@latitude = @lat_lng.at(0)
