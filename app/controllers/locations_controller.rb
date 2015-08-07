2a52b07223b66089162352a764edce14d676078c
Cameron Steer <cdsteer@csltliamb.swan.ac.uk>
2015-08-07 09:30:46 +0100

    fixed maybe...

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -89,8 +89,8 @@ class LocationsController < ApplicationController
 			x = User.count
 			puts x
 			for i in 1..x
-				myUser = User.find(i)
-				tree.insert([myUser.location.latitude, myUser.location.longitude], i)
+				@myUser = User.find(i)
+				tree.insert([@myUser.location.latitude, @myUser.location.longitude], i)
 			end
 			return tree
 		end
